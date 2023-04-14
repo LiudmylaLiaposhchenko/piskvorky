20efdb246f22c8807f151775f0972d8bf98f0c4d
Liudmyla Liaposhchenko <liaposhchenkol@gmail.com>
2023-04-14 19:17:31 +0200

    Předani funkci findWinner.

--- hra.js
+++ hra.js
@@ -1,20 +1,7 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
-
-const handleClick = (event) => {
-  if (currentPlayer === 'circle') {
-    event.target.classList.add('board__field--circle');
-    currentPlayer = 'cross';
-    document.querySelector('.player').classList.remove('board__field--circle');
-    document.querySelector('.player').classList.add('board__field--cross');
-  } else {
-    event.target.classList.add('board__field--cross');
-    currentPlayer = 'circle';
-    document.querySelector('.player').classList.remove('board__field--cross');
-    document.querySelector('.player').classList.add('board__field--circle');
-  }
-
-  event.target.disabled = true;
-};
+let button = '_';
 
 document.querySelector('.restart').addEventListener('click', (event) => {
   const odpovedUzivatele = confirm('Opravdu chceš začít znovu?');
@@ -23,7 +10,134 @@ document.querySelector('.restart').addEventListener('click', (event) => {
   }
 });
 
+const herniPole = [
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+  '_',
+];
+
 const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
+buttons.forEach((button, index) => {
+  const handleClick = (event) => {
+    if (currentPlayer === 'circle') {
+      herniPole[index] = 'o';
+      event.target.classList.add('board__field--circle');
+      currentPlayer = 'cross';
+      document
+        .querySelector('.player')
+        .classList.remove('board__field--circle');
+      document.querySelector('.player').classList.add('board__field--cross');
+    } else {
+      herniPole[index] = 'x';
+      event.target.classList.add('board__field--cross');
+      currentPlayer = 'circle';
+      document.querySelector('.player').classList.remove('board__field--cross');
+      document.querySelector('.player').classList.add('board__field--circle');
+    }
+    event.target.disabled = true;
+    const vitez = findWinner(herniPole);
+    if (vitez === 'o' || vitez === 'x') {
+      alert(`Vyhrál hráč se symbolem ${vitez}!`);
+      location.reload();
+    }
+  };
+
   button.addEventListener('click', handleClick);
 });
